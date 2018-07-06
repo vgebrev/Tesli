53a6b00abc1e994b34450510dcd523746f92e98d
Veli Gebrev <vgebrev@gmail.com>
2018-07-06 23:41:43 +0200

    calendar header look'n'feel

--- front-end/src/app/components/calendar/calendar-header/calendar-header.component.ts
+++ front-end/src/app/components/calendar/calendar-header/calendar-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-calendar-header',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar-header.component.scss']
 })
 export class CalendarHeaderComponent implements OnInit {
+  
+  @Input() view: string;
+
+  @Input() viewDate: Date;
+
+  @Input() locale: string = 'en';
+
+  @Output() viewChange: EventEmitter<string> = new EventEmitter();
+
+  @Output() viewDateChange: EventEmitter<Date> = new EventEmitter();
 
   constructor() { }
 
