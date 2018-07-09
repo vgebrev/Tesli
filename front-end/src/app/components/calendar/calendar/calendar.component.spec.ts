188858821c9812a799a7b17fe157d70c5008468b
Veli Gebrev <vgebrev@gmail.com>
2018-07-09 23:51:33 +0200

    added stubs to fix calendar test skeleton

--- front-end/src/app/components/calendar/calendar/calendar.component.spec.ts
+++ front-end/src/app/components/calendar/calendar/calendar.component.spec.ts
@@ -1,6 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input } from '@angular/core';
 
 import { CalendarComponent } from './calendar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from '../../../modules/app-material.module';
+
+@Component({ selector: 'app-calendar-header', template: ''})
+class CalendarHeaderComponentStub {
+  @Input() view: string;
+  @Input() viewDate: Date;
+}
+
+@Component({selector: 'mwl-calendar-month-view', template: ''})
+class CalendarMonthViewComponentStub {  
+  @Input() viewDate: Date;
+  @Input() events = []; 
+  @Input() activeDayIsOpen: boolean = false;
+}
+
+@Component({selector: 'mwl-calendar-week-view', template: ''})
+class CalendarWeekViewComponentStub {  
+  @Input() viewDate: Date;
+  @Input() events = []; 
+}
+
+@Component({selector: 'mwl-calendar-day-view', template: ''})
+class CalendarDayViewComponentStub {  
+  @Input() viewDate: Date;
+  @Input() events = []; 
+}
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
@@ -8,7 +36,17 @@ describe('CalendarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalendarComponent ]
+      imports: [
+        NoopAnimationsModule,
+        AppMaterialModule
+      ],
+      declarations: [ 
+        CalendarComponent,
+        CalendarHeaderComponentStub,
+        CalendarMonthViewComponentStub,
+        CalendarWeekViewComponentStub,
+        CalendarDayViewComponentStub
+      ]
     })
     .compileComponents();
   }));
