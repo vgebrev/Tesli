6d0944ce05871412831afb09148e70037a9e8510
Veli Gebrev <vgebrev@gmail.com>
2018-07-09 23:37:56 +0200

    added stubs to fix calendar-header test skeleton

--- front-end/src/app/components/calendar/calendar-header/calendar-header.component.spec.ts
+++ front-end/src/app/components/calendar/calendar-header/calendar-header.component.spec.ts
@@ -1,6 +1,44 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalendarHeaderComponent } from './calendar-header.component';
+import { Directive, Input, Output, HostListener, EventEmitter, Pipe, PipeTransform } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from 'src/app/modules/app-material.module';
+
+@Directive({
+  selector: '[mwlCalendarPreviousView]'
+})
+class CalendarPreviousViewDirectiveStub {
+  @Input() view: string;
+  @Input() viewDate: Date;
+  @Output() viewDateChange: EventEmitter<Date> = new EventEmitter();
+  @HostListener('click')
+  onClick(): void {}
+}
+
+@Directive({ selector: '[mwlCalendarNextView]' })
+class CalendarNextViewDirectiveStub {
+  @Input() view: string;
+  @Input() viewDate: Date;
+  @Output() viewDateChange: EventEmitter<Date> = new EventEmitter();
+  @HostListener('click')
+  onClick(): void {}
+}
+
+@Directive({ selector: '[mwlCalendarToday]' })
+class CalendarTodayDirectiveStub {
+  @Input() viewDate: Date;
+  @Output() viewDateChange: EventEmitter<Date> = new EventEmitter();
+  @HostListener('click')
+  onClick(): void { }
+}
+
+@Pipe({ name: 'calendarDate' })
+class CalendarDatePipeStub implements PipeTransform {
+  transform(): string {
+    return null;
+  }
+}
 
 describe('CalendarHeaderComponent', () => {
   let component: CalendarHeaderComponent;
@@ -8,7 +46,16 @@ describe('CalendarHeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalendarHeaderComponent ]
+      imports: [
+        NoopAnimationsModule,
+        AppMaterialModule
+      ],
+      declarations: [ 
+        CalendarHeaderComponent, 
+        CalendarPreviousViewDirectiveStub,
+        CalendarNextViewDirectiveStub,
+        CalendarTodayDirectiveStub,
+        CalendarDatePipeStub ]
     })
     .compileComponents();
   }));
