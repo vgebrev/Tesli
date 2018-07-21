448f0c36660b6bf9a57be3c6d97311563e5ae1c4
Veli Gebrev <vgebrev@gmail.com>
2018-07-21 10:49:50 +0200

    fix(lesson editor): failing test due to unconfigured dependencies

--- front-end/src/app/components/calendar/lesson-editor/lesson-editor.component.spec.ts
+++ front-end/src/app/components/calendar/lesson-editor/lesson-editor.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { LessonEditorComponent } from './lesson-editor.component';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AppMaterialModule } from '../../../modules/app-material.module';
+import { AmazingTimePickerService } from 'amazing-time-picker';
 
 class MatDialogRefMock {
   constructor() { }
@@ -12,15 +14,18 @@ describe('LessonEditorComponent', () => {
   let fixture: ComponentFixture<LessonEditorComponent>;
 
   beforeEach(async(() => {
+    const amazingTimePickerServiceSpy = jasmine.createSpyObj<AmazingTimePickerService>(['open']);
     TestBed.configureTestingModule({
       imports: [
         NoopAnimationsModule,
-        MatDialogModule
+        MatDialogModule,
+        AppMaterialModule
       ],
       declarations: [ LessonEditorComponent ],
       providers: [
         { provide: MatDialogRef, useValue: MatDialogRefMock },
-        { provide: MAT_DIALOG_DATA, useValue: { data: { eventStart: new Date() } } }
+        { provide: MAT_DIALOG_DATA, useValue: { data: { eventStart: new Date() } } },
+        { provide: AmazingTimePickerService, useValue: amazingTimePickerServiceSpy }
       ]
     })
     .compileComponents();
