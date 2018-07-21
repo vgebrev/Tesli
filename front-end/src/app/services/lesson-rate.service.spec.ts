971a5ef0949934b0370dda4cd43147409d892a2c
Veli Gebrev <vgebrev@gmail.com>
2018-07-21 12:32:14 +0200

    test(lesson rate service): add 2016 rates

--- front-end/src/app/services/lesson-rate.service.spec.ts
+++ front-end/src/app/services/lesson-rate.service.spec.ts
@@ -15,9 +15,18 @@ describe('LessonRateService', () => {
 
   it('getPrice returns the correct price for a given date and number of students',
   inject([LessonRateService], (service: LessonRateService) => {
+    const jan1st2016 = new Date(2016, 0, 1);
     const jan1st2017 = new Date(2017, 0, 1);
     const jan1st2018 = new Date(2018, 0, 1);
     const theory = [
+      { date: jan1st2016, students: 1, expectedPrice: 300 },
+      { date: jan1st2016, students: 2, expectedPrice: 250 },
+      { date: jan1st2016, students: 3, expectedPrice: 200 },
+      { date: jan1st2016, students: 4, expectedPrice: 150 },
+      { date: jan1st2016, students: 5, expectedPrice: 150 },
+      { date: jan1st2016, students: 6, expectedPrice: 150 },
+      { date: jan1st2016, students: 7, expectedPrice: 100 },
+      { date: jan1st2016, students: 8, expectedPrice: 100 },
       { date: jan1st2017, students: 1, expectedPrice: 300 },
       { date: jan1st2017, students: 2, expectedPrice: 230 },
       { date: jan1st2017, students: 3, expectedPrice: 180 },
