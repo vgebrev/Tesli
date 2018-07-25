1add55dd72e1ba3c318e6f572c65543edae77625
Veli Gebrev <vgebrev@gmail.com>
2018-07-25 07:47:44 +0200

    refactor(validators): wrap time data and helpers into a class

---
+++ front-end/src/app/model/time.ts
@@ -0,0 +1,26 @@
+export class Time {
+    isValid: boolean;
+    hour?: number;
+    minute?: number;
+    constructor(time: string) {
+        this.parse(time);
+    }
+
+    parse(time: string) {
+        if (time === null) {
+            this.isValid = false;
+            return false;
+        }
+
+        const parts = time.split(':');
+        this.hour = +parts[0];
+        this.minute = +parts[1];
+
+        this.isValid = this.hour >= 0 && this.hour <= 23 && this.minute >= 0 && this.minute <= 59;
+        return this.isValid;
+    }
+
+    isBefore(time: Time) {
+      return this.hour < time.hour || (this.hour === time.hour && this.minute < time.minute);
+    }
+}
