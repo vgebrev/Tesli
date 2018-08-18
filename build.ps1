5638effe62fdb9e9af44d873ce037a247b5d98dd
Veli Gebrev <vgebrev@gmail.com>
2018-08-18 17:50:10 +0200

    chore(build): add database update step to build

--- build.ps1
+++ build.ps1
@@ -7,26 +7,39 @@ param (
 
 Write-Output "Building $config";
 
+#Back-end tests
 dotnet test .\back-end\Tesli.Tests\Tesli.Tests.csproj;
-Write-Output "dotnet test exited with code: $LastExitCode";
+Write-Output "Back-end tests step exited with code: $LastExitCode";
 if ($LastExitCode -ne 0) { exit $LastExitCode; }
 
+#Front-end tests
 Push-Location .\front-end
 ng test --watch=false
-Write-Output "ng test exited with code: $LastExitCode"
+Write-Output "Front-end tests step exited with code: $LastExitCode"
 if ($LastExitCode -ne 0) { Pop-Location; exit $LastExitCode; }
 Pop-Location;
 
+#Back-end publish
 dotnet publish .\back-end\Tesli.Api --configuration $config --output $deployRoot\back-end\api;
-Write-Output "dotnet publish exited with code: $LastExitCode";
+Write-Output "Back-end publish step exited with code: $LastExitCode";
 if ($LastExitCode -ne 0) { exit $LastExitCode; }
 
+#Database update
+Push-Location .\back-end\Tesli.Database.Sqlite
+dotnet run "$deployRoot\back-end\api\data\tesli.$config.db"
+Write-Output "Database update step exited with code: $LastExitCode";
+if ($LastExitCode -ne 0) { exit $LastExitCode; }
+Pop-Location;
+
+#Front-end build
 Push-Location .\front-end
 ng build --configuration $config
 Pop-Location; 
-Write-Output "ng build exited with code: $LastExitCode";
+Write-Output "Front-end build step exited with code: $LastExitCode";
 if ($LastExitCode -ne 0) { exit $LastExitCode; }
 
+#Front-end publish
 Remove-Item $deployRoot\front-end\*.*;
 Copy-Item .\front-end\dist\Tesli\*.* -Destination $deployRoot\front-end -Force;
+Write-Output "Front-end publish step exited with code: $LastExitCode";
 exit $LastExitCode;
