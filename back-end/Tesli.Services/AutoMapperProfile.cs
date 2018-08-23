e69bb375a42dfb506c0796ac6b207a5150c2f065
Veli Gebrev <vgebrev@gmail.com>
2018-08-23 23:34:06 +0200

    fix: convert lesson date to local time before storing in the db

--- back-end/Tesli.Services/AutoMapperProfile.cs
+++ back-end/Tesli.Services/AutoMapperProfile.cs
@@ -14,9 +14,9 @@ namespace Tesli.Services
                 .ForMember(destination => destination.Id, map => map.Ignore());
             CreateMap<Lesson, Lesson>()
                 .ForMember(destination => destination.Id, map => map.Ignore())
-                .ForMember(destination => destination.LessonAttendees, map => map.Ignore());
+                .ForMember(destination => destination.LessonAttendees, map => map.Ignore())
+                .ForMember(destination => destination.Date, map => map.MapFrom(source => source.Date.ToLocalTime())); // TODO: Maybe this should be done as a custom model binder in the api
             CreateMap<LessonAttendee, LessonAttendee>()
-                //.ForMember(destination => destination.Id, map => map.Ignore())
                 .ForMember(destination => destination.Student, map => map.Ignore());
         }
     }
